e53a71c02db4974aca6ee003583fe5e92ddeac50
John Day <john.day@mybobs.com>
2024-11-25 15:52:40 -0600

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  compressHTML: true,
+  site: 'https://studiowxy.com',
+  output: 'static',
+
+
   integrations: [react(), tailwind()]
 });
